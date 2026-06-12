4af737354322c01562b9c9b85a6f367c7e5a6936
M5R DEV <128253384+M5RDEV@users.noreply.github.com>
2026-06-12 21:18:53 +0300

    Add files via upload

---
+++ Script/firebase.js
@@ -0,0 +1,25 @@
+window.MUSLIM_ENCYCLOPEDIA_FIREBASE = {
+    config: {
+        apiKey: "AIzaSyAiMfMsgtMKaqTeJkKzpnca0wUmoSEpglA",
+        authDomain: "muslimencycloedia-website.firebaseapp.com",
+        databaseURL: "https://muslimencycloedia-website-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "muslimencycloedia-website",
+        storageBucket: "muslimencycloedia-website.firebasestorage.app",
+        messagingSenderId: "568815726247",
+        appId: "1:568815726247:web:22f8533f9996ed0bb03582"
+    },
+    entries: {
+        fullofflineCount: {
+            endpoint: "FullOffline"
+        },
+        offlineCount: {
+            endpoint: "Offline"
+        },
+        onlineCount: {
+            endpoint: "Online"
+        },
+        websiteCount: {
+            endpoint: "Website"
+        }
+    }
+};
